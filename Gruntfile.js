4f7d198c8738d095a5fccaa113c289a20b30a52f
paladox <thomasmulhall410@yahoo.com>
2015-07-30 19:36:23 +0400

    Update tests in cargo extensions
    Adds support for running npm in the extension. This is a new test and
    upgrades from deprecated jshint. jshint isent actualy deprecated just that
    tey want you too upgrade to npm.
    
    Adds a composer file for running php code sniffer ( i will do a follow up
    with php code sniffing )
    
    Required-by: https://gerrit.wikimedia.org/r/#/c/228473/
    
    Change-Id: I6b32d6d8fb58cecb69a7485d59a1f06b3d873849

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+/*jshint node:true */
+module.exports = function ( grunt ) {
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-jscs' );
+
+	grunt.initConfig( {
+		jshint: {
+			options: {
+				jshintrc: true
+			},
+			all: [
+				'*.js'
+			]
+		},
+		jscs: {
+			src: '<%= jshint.all %>'
+		},
+		banana: {
+			all: 'i18n/'
+		},
+		jsonlint: {
+			all: [
+				'**/*.json',
+				'!node_modules/**'
+			]
+		}
+	} );
+
+	grunt.registerTask( 'test', [ 'jshint', 'jscs', 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'default', 'test' );
+};
