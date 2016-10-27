e481165d6b20325d4f41fc11d5a8b2b4ed74ae76
Yaron Koren <yaron57@gmail.com>
2016-10-27 17:00:49 +0000

    Possible fix for drilldown number-ranges bug
    Change-Id: I87a6d45ff923757bac17ed1b2541801bb53d373e

--- drilldown/CargoSpecialDrilldown.php
+++ drilldown/CargoSpecialDrilldown.php
@@ -661,7 +661,7 @@ END;
 		for ( $i = 0; $i < count( $numberArray ); $i++ ) {
 			if ( $curSeparator < count( $propertyValues ) - 1 ) {
 				$curNumber = $numberArray[$i];
-				while ( $curNumber >= $bucketSeparators[$curSeparator + 1] ) {
+				while ( ( $curSeparator < count( $bucketSeparators) ) && ( $curNumber >= $bucketSeparators[$curSeparator + 1] ) ) {
 					$curSeparator++;
 				}
 			}
