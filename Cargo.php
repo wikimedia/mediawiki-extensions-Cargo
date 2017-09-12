7ae6cfb39671ff1c167a36e6da3da1046ef50474
Yaron Koren <yaron57@gmail.com>
2017-09-12 13:31:03 +0000

    Version 1.4
    Change-Id: Ib7f440119e37892974fc042262c9e3ebcf5b2197

--- Cargo.php
+++ Cargo.php
@@ -25,7 +25,7 @@ if ( version_compare( $GLOBALS['wgVersion'], '1.27', '>=' ) ) {
 
 // All the rest is for backward compatibility, for MW 1.26 and lower.
 
-define( 'CARGO_VERSION', '1.3.1' );
+define( 'CARGO_VERSION', '1.4' );
 
 $wgExtensionCredits['parserhook'][] = array(
 	'path' => __FILE__,
