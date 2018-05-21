bd9d97d9989aec2d100f56b3f499cdd0d962c153
Yaron Koren <yaron57@gmail.com>
2018-05-21 01:00:04 +0000

    Added @covers tag for unit test
    Change-Id: I69210eee835cdaf4ff1a4c8faf3692a04c968efe

--- tests/phpunit/CargoUtilsTest.php
+++ tests/phpunit/CargoUtilsTest.php
@@ -2,6 +2,9 @@
 
 class CargoUtilsTest extends MediaWikiTestCase {
 
+	/**
+	 * @covers CargoUtils::smartSplit
+	 */
 	public function testSmartSplit() {
 		$this->assertEquals( array(), CargoUtils::smartSplit( ',', '' ) );
 		$this->assertEquals( array( 'one', 'two' ), CargoUtils::smartSplit( ',', 'one,two' ) );
