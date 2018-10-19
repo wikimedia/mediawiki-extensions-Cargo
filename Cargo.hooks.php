dec2d3abffff2d58fe5fd00b980e5c17b327a3ab
Yaron Koren <yaron57@gmail.com>
2018-10-19 16:39:22 +0000

    Version 2.0
    Change-Id: I40c07f144845c96d2513d00690d60524b1bdeec2

--- Cargo.hooks.php
+++ Cargo.hooks.php
@@ -11,7 +11,7 @@ class CargoHooks {
 	public static function registerExtension() {
 		global $cgScriptPath, $wgScriptPath, $wgCargoFieldTypes, $wgGroupPermissions;
 
-		define( 'CARGO_VERSION', '2.0-rc1' );
+		define( 'CARGO_VERSION', '2.0' );
 
 		// Script path.
 		$cgScriptPath = $wgScriptPath . '/extensions/Cargo';
--- Cargo.php
+++ Cargo.php
@@ -25,7 +25,7 @@ if ( version_compare( $GLOBALS['wgVersion'], '1.27', '>=' ) ) {
 
 // All the rest is for backward compatibility, for MW 1.26 and lower.
 
-define( 'CARGO_VERSION', '2.0-rc1' );
+define( 'CARGO_VERSION', '2.0' );
 
 $wgExtensionCredits['parserhook'][] = array(
 	'path' => __FILE__,
