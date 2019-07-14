1277362b8e78308dd0a36c933439de145e5906f7
Yaron Koren <yaron57@gmail.com>
2019-07-14 19:38:40 +0000

    Fix for handling 0 values in drilldown
    Change-Id: Ib341a815da2cea7a3a28bf1ca6b0de3dc49f012f

--- drilldown/CargoAppliedFilter.php
+++ drilldown/CargoAppliedFilter.php
@@ -144,7 +144,9 @@ class CargoAppliedFilter {
 					$sql .= "( (" . $leftCond . ") AND (" . $rightCond . ") )";
 				}
 			} elseif ( $fv->is_none ) {
-				$checkNullOrEmptySql = ( $cdb->getType() == 'postgres' ? '' : "$value_field = '' OR " ) .
+				// For some reason, 0 values are treated as
+				// blank, so we need the "!= 0" check.
+				$checkNullOrEmptySql = ( $cdb->getType() == 'postgres' ? '' : "($value_field = '' AND $value_field != 0) OR " ) .
 					"$value_field IS NULL";
 				$sql .= "($checkNullOrEmptySql) ";
 			} elseif ( $this->filter->fieldDescription->isDateOrDatetime() ) {
