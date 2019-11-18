f2152fdbe095bd03ab7d1e9ad335fedff01e3f60
Yaron Koren <yaron57@gmail.com>
2019-11-18 16:20:11 +0000

    Fix for 7916f60028c3 - add missing $cdb setting
    Change-Id: I44598726afbf5a94e63424dc041f277c78ee175f

--- Cargo.hooks.php
+++ Cargo.hooks.php
@@ -242,6 +242,7 @@ class CargoHooks {
 		// this page to be saved by Cargo, since the page will be
 		// parsed right after this.
 		CargoStore::$settings['origin'] = 'Approved Revs revision approved';
+		$cdb = CargoUtils::getDB();
 		$useReplacementTable = $cdb->tableExists( '_pageData__NEXT' );
 		CargoPageData::storeValuesForPage( $title, $useReplacementTable );
 		$useReplacementTable = $cdb->tableExists( '_fileData__NEXT' );
@@ -262,6 +263,7 @@ class CargoHooks {
 			// No point storing the Cargo data if it's blank.
 			CargoStore::$settings['origin'] = 'Approved Revs revision unapproved';
 		}
+		$cdb = CargoUtils::getDB();
 		$useReplacementTable = $cdb->tableExists( '_pageData__NEXT' );
 		CargoPageData::storeValuesForPage( $title, $useReplacementTable, $egApprovedRevsBlankIfUnapproved );
 		$useReplacementTable = $cdb->tableExists( '_fileData__NEXT' );
