b1814542712689105c1eea500a0abc710c35e0dd
Yaron Koren <yaron57@gmail.com>
2020-01-09 18:27:49 +0000

    Version 2.4
    Change-Id: I757108d422b188d63cf60df67d12f9fff6c43a84

--- Cargo.hooks.php
+++ Cargo.hooks.php
@@ -11,7 +11,7 @@ class CargoHooks {
 	public static function registerExtension() {
 		global $cgScriptPath, $wgScriptPath, $wgCargoFieldTypes, $wgGroupPermissions;
 
-		define( 'CARGO_VERSION', '2.3.1' );
+		define( 'CARGO_VERSION', '2.4' );
 
 		// Script path.
 		$cgScriptPath = $wgScriptPath . '/extensions/Cargo';
