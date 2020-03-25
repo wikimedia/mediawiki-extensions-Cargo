f3e0891ba3f464199c08dd6d9c37d15e566d6b6c
Yaron Koren <yaron57@gmail.com>
2020-03-25 00:58:28 +0000

    Add slashes to characters not allowed in table and field names
    Change-Id: I9083d217117cfedb7c398144ccf55f0ee9f70afc

--- includes/parserfunctions/CargoDeclare.php
+++ includes/parserfunctions/CargoDeclare.php
@@ -84,8 +84,8 @@ class CargoDeclare {
 			return "Error: $type name \"$name\" ends with an underscore; this is not allowed.";
 		} elseif ( strpos( $name, '__' ) !== false ) {
 			return "Error: $type name \"$name\" contains more than one underscore in a row; this is not allowed.";
-		} elseif ( preg_match( '/[\.,\-<>(){}\[\]]/', $name ) ) {
-			return "Error: $type name \"$name\" cannot contain any of the following characters: .,-<>(){}[]";
+		} elseif ( preg_match( '/[\.,\-<>(){}\[\]\\\\\/]/', $name ) ) {
+			return "Error: $type name \"$name\" cannot contain any of the following characters: .,-<>(){}[]\/";
 		} elseif ( in_array( strtolower( $name ), self::$sqlReservedWords ) ) {
 			return "Error: \"$name\" cannot be used as a Cargo $type name, because it is an SQL keyword.";
 		} elseif ( in_array( strtolower( $name ), self::$cargoReservedWords ) ) {
