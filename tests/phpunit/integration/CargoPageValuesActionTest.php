ea5d72f2463a7cef0dcc658921a935b654740c38
Yaron Koren <yaron57@gmail.com>
2020-06-11 01:33:04 +0000

    Improve PageValuesActionTest
    Change-Id: Icf5adad8e7c31403752e0ed4285b58436b8cccf2

--- tests/phpunit/integration/CargoPageValuesActionTest.php
+++ tests/phpunit/integration/CargoPageValuesActionTest.php
@@ -16,9 +16,9 @@ class CargoPageValuesActionTest extends MediaWikiIntegrationTestCase {
 	}
 
 	/**
-	 * Mock BaseTemplate for use in ::addLink test.
+	 * Mock Skin for use in ::addLink test.
 	 * @param Title $title
-	 * @return BaseTemplate
+	 * @return Skin
 	 */
 	private function getSkin( Title $title ) {
 		$skin = $this->getMockBuilder( Skin::class )
@@ -37,12 +37,18 @@ class CargoPageValuesActionTest extends MediaWikiIntegrationTestCase {
 	 */
 	public function testAddLink( $title ) {
 		$sidebar = [];
-		$actual = CargoPageValuesAction::addLink(
+		$result = CargoPageValuesAction::addLink(
 			$this->getSkin( $title ),
 			$sidebar
 		);
 
-		// @TODO - need a good check here
+		if ( $title->getNamespace() == NS_SPECIAL ) {
+			$this->assertFalse( $result );
+			$this->assertCount( 0, $sidebar );
+		} else {
+			$this->assertNull( $result );
+			$this->assertArrayHasKey( 'cargo-pagevalues', $sidebar['TOOLBOX'] );
+		}
 	}
 
 	/** @return array */
