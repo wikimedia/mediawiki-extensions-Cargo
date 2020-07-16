03305feba5fc21cc309506c26ef0d80309b09747
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2020-07-16 19:16:30 +0000

    build: Updating npm dependencies
    * eslint-config-wikimedia: 0.15.3 → 0.16.2
      The following rules are failing and were disabled:
      * array-callback-return
      * es/no-object-assign
    
    * grunt-eslint: 22.0.0 → 23.0.0
    
    Change-Id: Ib34d824feab15422b7d49efb03043b53c69bf291

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,6 @@ module.exports = function ( grunt ) {
 	grunt.loadNpmTasks( 'grunt-stylelint' );
 
 	grunt.initConfig( {
-		// eslint-disable-next-line no-restricted-properties
 		banana: Object.assign(
 			conf.MessagesDirs,
 			{
