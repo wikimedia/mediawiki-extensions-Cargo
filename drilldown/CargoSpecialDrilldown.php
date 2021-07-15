547dbe7e9ffef41c9d987cfa535d1388027c4a67
Yaron Koren <yaron57@gmail.com>
2021-07-15 14:11:17 +0000

    Remove CargoDrilldownIEFixes.css
    IE fixes for jQuery UI - this file is now doubly unnecessary.
    
    Change-Id: Id3cfc503ad1daf8aaef8701011a9e4e977b2a3a7

--- drilldown/CargoSpecialDrilldown.php
+++ drilldown/CargoSpecialDrilldown.php
@@ -22,8 +22,7 @@ class CargoSpecialDrilldown extends IncludableSpecialPage {
 	}
 
 	public function execute( $query ) {
-		global $cgScriptPath, $wgCargoPageDataColumns;
-		global $wgCargoFileDataColumns;
+		global $wgCargoPageDataColumns, $wgCargoFileDataColumns;
 
 		$this->checkPermissions();
 
@@ -36,8 +35,6 @@ class CargoSpecialDrilldown extends IncludableSpecialPage {
 		}
 		$this->setHeaders();
 		$out->addModules( 'ext.cargo.drilldown' );
-		$out->addScript( '<!--[if IE]><link rel="stylesheet" href="' . $cgScriptPath .
-			'/drilldown/resources/CargoDrilldownIEFixes.css" media="screen" /><![endif]-->' );
 
 		$queryparts = explode( '/', $query, 1 );
 		$mainTable = isset( $queryparts[0] ) ? $queryparts[0] : '';
