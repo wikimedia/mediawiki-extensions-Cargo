70546fbf48e3cf65d05e9def7a39e2a6c45200a7
Umherirrender <umherirrender_de.wp@web.de>
2021-12-06 16:30:25 +0100

    Emit deprecation warning from php entry point
    Change-Id: I0f797290009be25b9258218a8fc126be3a646aa1

--- Cargo.php
+++ Cargo.php
@@ -11,7 +11,7 @@ wfLoadExtension( 'Cargo' );
 $wgMessagesDirs['Cargo'] = __DIR__ . '/i18n';
 $wgExtensionMessagesFiles['CargoMagic'] = __DIR__ . '/Cargo.i18n.magic.php';
 $wgExtensionMessagesFiles['CargoAlias'] = __DIR__ . '/Cargo.alias.php';
-/* wfWarn(
+wfWarn(
 	'Deprecated PHP entry point used for Cargo extension. Please use wfLoadExtension instead, ' .
 	'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-); */
+);
