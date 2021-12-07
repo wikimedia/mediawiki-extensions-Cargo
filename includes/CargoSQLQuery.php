d9b9ae09a160986c638b926a9633d8c4d7b68379
sanjay-thiyagarajan <sanjayipscoc@gmail.com>
2021-12-07 17:51:59 +0530

    Add unicode replacement for '#' in query "where" string
    Change-Id: I6f278f9fd14e3350ea21fc447e553d6fa6c6c806

--- includes/CargoSQLQuery.php
+++ includes/CargoSQLQuery.php
@@ -114,7 +114,8 @@ class CargoSQLQuery {
 			'/\-\-/' => '--',
 			'/#/' => '#',
 		];
-
+		// Replace # with corresponding Unicode value to prevent security leaks.
+		$whereStr = str_replace( '#', '\u0023', $whereStr );
 		// HTML-decode the string - this is necessary if the query
 		// contains a call to {{PAGENAME}} and the page name has any
 		// special characters, because {{PAGENAME]] unfortunately
