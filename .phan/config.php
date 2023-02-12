9d9ff65fa798612e72bc000e145a2aef5ad20168
Máté Szabó <mszabo@fandom.com>
2023-02-12 22:37:00 +0100

    Add Phan configuration
    Phan is extensively used for static analysis in MediaWiki itself as well
    as many extensions. It is fairly capable of detecting simple regressions
    such as the invalid property access introduced in 3d876fc, as well as
    more complex problems. So, let's add a basic Phan configuration to
    Cargo.
    
    Adding Phan to an existing codebase inevitably generates a large set of
    existing issues, some real and some false positives. To avoid having to
    make sweeping changes straight off the bat to satisfy Phan, exclude
    issue types currently detected in the codebase from analysis. This does
    make Phan analysis somewhat less useful as-is, but this allows for
    incrementally strengthening the analysis by fixing and enabling issues
    one by one. Alternatively, we could use a baseline file[1] - this is
    certainly possible but does not seem to be often used in extensions, and
    could create some confusion if some new code following an existing
    pattern suddenly results in Phan errors.
    
    Even with the current, limited, analysis configuration, Phan would have
    caught the 3d876fc regression. Testing by adding
    `$foo = $mappingFormat->mParser;` to line 72 of CargoDisplayMap.php
    yields:
    ```
    includes/parserfunctions/CargoDisplayMap.php:72 PhanAccessPropertyProtected Cannot access protected property \CargoGoogleMapsFormat->mParser defined at includes/formats/CargoDisplayFormat.php:12
    ```
    
    [1] https://github.com/phan/phan/wiki/Tutorial-for-Analyzing-a-Large-Sloppy-Code-Base#setting-up-a-phan-baseline
    
    Bug: T329466
    Change-Id: Ifdf089bf3d5a6fc1b91de610faef4557a48fb3c7

---
+++ .phan/config.php
@@ -0,0 +1,56 @@
+<?php
+$config = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+
+// Ensure Phan doesn't try to suggest PHP language features not supported
+// by the minimum required PHP version of the minimum supported MediaWiki version (1.35).
+$config['minimum_target_php_version'] = '7.3.19';
+
+$config['suppress_issue_types'] = array_merge(
+	$config['suppress_issue_types'],
+	[
+		// Suppress issue types that currently exist in the codebase.
+		// This means that Phan initially won't do much, but it allows for
+		// checks to be incrementally fixed and enabled without massive changes.
+		'PhanDeprecatedPartiallySupportedCallable',
+		'PhanImpossibleCondition',
+		'PhanImpossibleTypeComparison',
+		'PhanNonClassMethodCall',
+		'PhanParamSignatureMismatch',
+		'PhanPluginInvalidPregRegex',
+		'PhanPluginLoopVariableReuse',
+		'PhanPluginRedundantAssignment',
+		'PhanPossiblyUndeclaredVariable',
+		'PhanStaticCallToNonStatic',
+		'PhanTypeArraySuspicious',
+		'PhanTypeArraySuspiciousNullable',
+		'PhanTypeInvalidLeftOperandOfAdd',
+		'PhanTypeInvalidLeftOperandOfNumericOp',
+		'PhanTypeMismatchArgument',
+		'PhanTypeMismatchArgumentInternal',
+		'PhanTypeMismatchArgumentNullable',
+		'PhanTypeMismatchArgumentNullableInternal',
+		'PhanTypeMismatchArgumentProbablyReal',
+		'PhanTypeMismatchDimAssignment',
+		'PhanTypeMismatchDimFetchNullable',
+		'PhanTypeMismatchReturn',
+		'PhanTypeMismatchReturnProbablyReal',
+		'PhanTypeMissingReturn',
+		'PhanTypePossiblyInvalidDimOffset',
+		'PhanUndeclaredClassInstanceof',
+		'PhanUndeclaredClassMethod',
+		'PhanUndeclaredConstant',
+		'PhanUndeclaredExtendedClass',
+		'PhanUndeclaredMethod',
+		'PhanUndeclaredProperty',
+		'PhanUndeclaredStaticMethod',
+		'PhanUndeclaredTypeThrowsType',
+		'PhanUndeclaredVariable',
+		'PhanUndeclaredVariableAssignOp',
+		'PhanUndeclaredVariableDim',
+		'SecurityCheck-DoubleEscaped',
+		'SecurityCheck-SQLInjection',
+		'SecurityCheck-XSS',
+	]
+);
+
+return $config;
