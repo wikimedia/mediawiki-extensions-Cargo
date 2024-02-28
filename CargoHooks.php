9c60a3f2f36a188cfae53e229f3b93afc24f8713
Yaron Koren <yaron57@gmail.com>
2024-02-28 19:25:17 +0000

    Version 3.5
    Change-Id: I591e3ec5e13cf9f4548ffc404790617134aacf81

--- CargoHooks.php
+++ CargoHooks.php
@@ -16,7 +16,7 @@ use MediaWiki\User\UserIdentity;
 class CargoHooks {
 
 	public static function registerExtension() {
-		define( 'CARGO_VERSION', '3.5-alpha' );
+		define( 'CARGO_VERSION', '3.5' );
 	}
 
 	public static function initialize() {
