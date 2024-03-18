851bc48f57d3dc27ddf04d93055808046d8d27ca
Umherirrender <umherirrender_de.wp@web.de>
2024-03-18 18:39:41 +0100

    build: Suppress phan issue with IReadableDatabase
    Follow-up: I01a1aa0ddbc756226d8e3404ac1cc3350ad5df64
    Change-Id: I5a0f7f22ef87105c70c0d5d37822f265b5558e53

--- includes/CargoUtils.php
+++ includes/CargoUtils.php
@@ -96,6 +96,9 @@ class CargoUtils {
 		$lbFactory = MediaWikiServices::getInstance()->getDBLoadBalancerFactory();
 		if ( method_exists( $lbFactory, 'getReplicaDatabase' ) ) {
 			// MW 1.40+
+			// The correct type \Wikimedia\Rdbms\IReadableDatabase cannot be used
+			// as return type, as that class only exists since 1.40.
+			// @phan-suppress-next-line PhanTypeMismatchReturnSuperType
 			return $lbFactory->getReplicaDatabase();
 		} else {
 			return $lbFactory->getMainLB()->getConnection( DB_REPLICA );
