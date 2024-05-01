f890b4e44101455b2f2a0cf3d6390d0da8c4f329
xtex <xtexchooser@duck.com>
2024-05-01 15:51:23 +0800

    Cargo: Add special page aliases for zh-hans
    Change-Id: I579ea8c0b4c078d7e614cb703e3b17cb985b1e8d

--- Cargo.alias.php
+++ Cargo.alias.php
@@ -20,3 +20,16 @@ $specialPageAliases['en'] = [
 	'SwitchCargoTable' => [ 'SwitchCargoTable' ],
 	'RecreateCargoData' => [ 'RecreateCargoData' ],
 ];
+
+/** Simplified Chinese (中文（简体）) */
+$specialPageAliases['zh-hans'] = [
+	'CargoExport' => [ 'Cargo导出' ],
+	'CargoTables' => [ 'Cargo表' ],
+	'DeleteCargoTable' => [ '删除Cargo表' ],
+	'Drilldown' => [ '深入分析' ],
+	'CargoTableDiagram' => [ 'Cargo图表' ],
+	'PageValues' => [ '页面值' ],
+	'CargoQuery' => [ 'Cargo查询', '查看数据' ],
+	'SwitchCargoTable' => [ '切换Cargo表' ],
+	'RecreateCargoData' => [ '重新创建Cargo数据' ],
+];
