c1cd451b6417ee4797f50f81cf68b767cf51a58c
xtex <xtexchooser@duck.com>
2024-05-01 16:14:00 +0800

    Cargo: Add magic word aliases for zh-hans
    Change-Id: I6845f991d4259b65ad9bba6f87ade150c50c8161

--- Cargo.i18n.magic.php
+++ Cargo.i18n.magic.php
@@ -15,3 +15,14 @@ $magicWords['en'] = [
 	'cargo_display_map' => [ 0, 'cargo_display_map' ],
 	'recurring_event' => [ 0, 'recurring_event' ],
 ];
+
+/** Simplified Chinese (中文（简体）) */
+$magicWords['zh-hans'] = [
+	'cargo_declare' => [ 0, 'cargo声明' ],
+	'cargo_attach' => [ 0, 'cargo附加' ],
+	'cargo_store' => [ 0, 'cargo保存' ],
+	'cargo_query' => [ 0, 'cargo查询' ],
+	'cargo_compound_query' => [ 0, 'cargo复合查询' ],
+	'cargo_display_map' => [ 0, 'cargo显示地图' ],
+	'recurring_event' => [ 0, '重复事件' ],
+];
