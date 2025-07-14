1ed26ace31fb97f481b49b1491603151162937ef
anterdc99 <xuminruixuan@live.cn>
2025-07-14 17:21:56 +0800

    Cargo: Update Chinese translations
    What:
    - Set Chinese primary special page alias to the English primary
    - Translate special pages into Traditional Chinese
    
    Change-Id: I342eddbdafcd422e940a0d0acecfff41dc14f003

--- Cargo.alias.php
+++ Cargo.alias.php
@@ -21,6 +21,19 @@ $specialPageAliases['en'] = [
 	'RecreateCargoData' => [ 'RecreateCargoData' ],
 ];
 
+/** Chinese (中文) */
+$specialPageAliases['zh'] = [
+	'CargoExport' => [ 'CargoExport' ],
+	'CargoTables' => [ 'CargoTables' ],
+	'DeleteCargoTable' => [ 'DeleteCargoTable' ],
+	'Drilldown' => [ 'Drilldown' ],
+	'CargoTableDiagram' => [ 'CargoTableDiagram' ],
+	'PageValues' => [ 'PageValues' ],
+	'CargoQuery' => [ 'CargoQuery' ],
+	'SwitchCargoTable' => [ 'SwitchCargoTable' ],
+	'RecreateCargoData' => [ 'RecreateCargoData' ],
+];
+
 /** Simplified Chinese (中文（简体）) */
 $specialPageAliases['zh-hans'] = [
 	'CargoExport' => [ 'Cargo导出' ],
@@ -32,4 +45,17 @@ $specialPageAliases['zh-hans'] = [
 	'CargoQuery' => [ 'Cargo查询', '查看数据' ],
 	'SwitchCargoTable' => [ '切换Cargo表' ],
 	'RecreateCargoData' => [ '重新创建Cargo数据' ],
+];
+
+/** Traditional Chinese (中文（繁體）) */
+$specialPageAliases['zh-hant'] = [
+	'CargoExport' => [ 'Cargo匯出' ],
+	'CargoTables' => [ 'Cargo表' ],
+	'DeleteCargoTable' => [ '刪除Cargo表' ],
+	'Drilldown' => [ '深入分析' ],
+	'CargoTableDiagram' => [ 'Cargo圖表' ],
+	'PageValues' => [ '頁面值' ],
+	'CargoQuery' => [ 'Cargo查詢', '檢視資料' ],
+	'SwitchCargoTable' => [ '切換Cargo表' ],
+	'RecreateCargoData' => [ '重新建立Cargo資料' ],
 ];
