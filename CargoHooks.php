7b138c91ba2a0bb710f507ac57bfc420f7b8fb21
Yaron Koren <yaron57@gmail.com>
2026-02-09 22:05:23 +0000

    Version 3.8.7
    Change-Id: I690bd5e689807e800f0cb7cf1cb7c37e76f55380

--- CargoHooks.php
+++ CargoHooks.php
@@ -18,7 +18,7 @@ use MediaWiki\User\UserIdentity;
 class CargoHooks {
 
 	public static function registerExtension() {
-		define( 'CARGO_VERSION', '3.8.6' );
+		define( 'CARGO_VERSION', '3.8.7' );
 	}
 
 	public static function initialize() {
